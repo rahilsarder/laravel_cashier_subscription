c65956c55693aac49d012a39103a17c2013a0c0f
Sarder Monowarul Amin <sarder.amin.rahil@gmail.com>
2022-05-24 02:45:28 +0600

    setup view for subscribe

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/subscribe', function () {
-    return 'Fucking Subscribe la!!';
+    return view('subscribe.index');
 });
